e60024b337e604773d77e59e28c94d4bb53fff8a
Amir Shevat <amir.shevat@gmail.com>
2016-08-19 05:14:07 -0700

    what?

--- index.js
+++ index.js
@@ -355,7 +355,7 @@ controller.hears('interactive', 'direct_message', function(bot, message) {
 controller.on('interactive_message_callback', function(bot, message) {
 
     // check message.actions and message.callback_id to see what action to take...
-    bot.replyPublic(message, "dont click my buttons!");
+    bot.replyInteractive(message, "dont click my buttons!");
     //bot.replyPrivate(message, "got message id"+message.callback_id+" and actions "+message.actions);
 
 
