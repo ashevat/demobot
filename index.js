c08278f6d7ebb0583434f09d42f42530c0d0273e
Amir Shevat <amir.shevat@gmail.com>
2016-08-19 04:13:48 -0700

    test interactive

--- index.js
+++ index.js
@@ -374,6 +374,45 @@ controller.hears('interactive', 'direct_message', function(bot, message) {
     });
 });
 
+// receive an interactive message, and reply with a message that will replace the original
+controller.on('interactive_message_callback', function(bot, message) {
+
+    // check message.actions and message.callback_id to see what action to take...
+
+    bot.replyInteractive(message, {
+        text: '...',
+        attachments: [
+            {
+                title: 'My buttons',
+                callback_id: '123',
+                attachment_type: 'default',
+                actions: [
+                    {
+                        "name":"yes",
+                        "text": "Yes!",
+                        "value": "yes",
+                        "type": "button",
+                    },
+                    {
+                        "text": "No!",
+                        "name": "no",
+                        "value": "delete",
+                        "style": "danger",
+                        "type": "button",
+                        "confirm": {
+                            "title": "Are you sure?",
+                            "text": "This will do something!",
+                            "ok_text": "Yes",
+                            "dismiss_text": "No"
+                        }
+                    }
+                ]
+            }
+        ]
+    });
+
+});
+
 
 controller.hears(['^hello$'], ['direct_message', 'direct_mention', "ambient"], function (bot, message) {
     loadPersonality(message.team, message.channel, function () {
