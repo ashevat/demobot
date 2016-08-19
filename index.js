4b3c019d1d60bb959d8a803147bb876912614e20
Amir Shevat <amir.shevat@gmail.com>
2016-08-19 04:41:12 -0700

    debug interactive

--- index.js
+++ index.js
@@ -378,38 +378,7 @@ controller.hears('interactive', 'direct_message', function(bot, message) {
 controller.on('interactive_message_callback', function(bot, message) {
 
     // check message.actions and message.callback_id to see what action to take...
-
-    bot.replyInteractive(message, {
-        text: '...',
-        attachments: [
-            {
-                title: 'My buttons',
-                callback_id: '123',
-                attachment_type: 'default',
-                actions: [
-                    {
-                        "name":"yes",
-                        "text": "Yes!",
-                        "value": "yes",
-                        "type": "button",
-                    },
-                    {
-                        "text": "No!",
-                        "name": "no",
-                        "value": "delete",
-                        "style": "danger",
-                        "type": "button",
-                        "confirm": {
-                            "title": "Are you sure?",
-                            "text": "This will do something!",
-                            "ok_text": "Yes",
-                            "dismiss_text": "No"
-                        }
-                    }
-                ]
-            }
-        ]
-    });
+    bot.reply(message, "got message id"+message.callback_id+" and actions "+message.actions);
 
 });
 
