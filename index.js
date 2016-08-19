f693ccc5c3be40dbfc10c0c48de299089bdada1e
Amir Shevat <amir.shevat@gmail.com>
2016-08-19 04:51:26 -0700

    fix in the right place.

--- index.js
+++ index.js
@@ -342,7 +342,7 @@ beepboop.on('botkit.rtm.started', function (bot, resource, meta) {
 
 
 controller.on('bot_channel_join', function (bot, message) {
-    bot.replyPrivate(message, ":wave: team -  I am your demo bot. \n I support direct mentions and DMs, I will read what is in this channel and try to respond accordingly -  you can also `@demobot: help` me.")
+    bot.reply(message, ":wave: team -  I am your demo bot. \n I support direct mentions and DMs, I will read what is in this channel and try to respond accordingly -  you can also `@demobot: help` me.")
 })
 
 
@@ -378,7 +378,8 @@ controller.hears('interactive', 'direct_message', function(bot, message) {
 controller.on('interactive_message_callback', function(bot, message) {
 
     // check message.actions and message.callback_id to see what action to take...
-    bot.reply(message, "got message id"+message.callback_id+" and actions "+message.actions);
+    bot.replyPrivate(message, "got message id"+message.callback_id+" and actions "+message.actions);
+
 
 });
 
