85243b69b7cf00e33dad298650d834e4ebd955b5
Amir Shevat <amir.shevat@gmail.com>
2016-08-19 04:56:47 -0700

    will it work?

--- index.js
+++ index.js
@@ -378,7 +378,8 @@ controller.hears('interactive', 'direct_message', function(bot, message) {
 controller.on('interactive_message_callback', function(bot, message) {
 
     // check message.actions and message.callback_id to see what action to take...
-    bot.replyPrivate(message, "got message id"+message.callback_id+" and actions "+message.actions);
+    bot.replyPrivate(message, "dont click my buttons!");
+    //bot.replyPrivate(message, "got message id"+message.callback_id+" and actions "+message.actions);
 
 
 });
