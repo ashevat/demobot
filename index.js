48339377a11014e9d7dd436e9c12894f93947ef9
Amir Shevat <amir.shevat@gmail.com>
2017-04-17 23:08:34 -0700

    fixed but not fully

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ controller.on('slash_command', function (bot, message) {
 
             }else {
                 var  attachments = null;
-                if(bot_say.indexOf('"attachments')>0){
+                if(bot_say.indexOf('attachments')>0){
                     console.log('match value "attachments'+ bot_say.indexOf('"attachments'));
                     try {
                     p_data = JSON.parse(bot_say);
